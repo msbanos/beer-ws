353d2ea531fb3bf126cb372a478cd16a112efbf0
Banoshe <msbanos@yahoo.com>
2018-10-03 18:15:58 -0600

    base recipe service

---
+++ src/main/java/banos/ms/beer/ws/RecipeService.java
@@ -0,0 +1,23 @@
+package banos.ms.beer.ws;
+
+import javax.ws.rs.Path;
+
+import banos.ms.beer.Recipe;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/03/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		Service layer to access recipe information.
+ */
+@Path("/recipes")
+public class RecipeService extends Service<Recipe> {
+	/**
+	 * Get the entity class.
+	 * @return The entity class.
+	 */
+	protected Class<Recipe> getEntityClass() {
+		return Recipe.class;
+	}
+}
